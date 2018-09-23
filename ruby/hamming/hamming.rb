74a34e4f2523643d581dc35ebac39349740795f4
hybridbad <hybridbad90@gmail.com>
2018-09-23 18:18:55 +0100

    recent save

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,3 +1,16 @@
-def Hamming(strand1, strand2)
-    puts "Nothing"
+
+class Hamming
+    def self.compute(normal, mutated)
+    #check strings are same length
+        unless normal.length == mutated.length
+            raise ArgumentError
+    end
+
+    #
+    gene_diff = 0
+    mutated_character = mutated.split("")
+    normal.each_char.with_index(0) do |char, index|
+        unless mutated_character[index] == char.to_s
+            gene_diff = gene_diff + 1
+    end
 end
