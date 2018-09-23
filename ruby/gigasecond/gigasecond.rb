06143ac42dd5348eb50610c2901f8c7b0c4e1d42
hybridbad <hybridbad90@gmail.com>
2018-09-23 18:43:41 +0100

    gigasecond exercise complete

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,2 +1,18 @@
+=begin
+# Gigasecond
+Calculate the moment when someone has lived for 10^9 seconds.
+
+A gigasecond is 10^9 (1,000,000,000) seconds.
+=end
+
+
 class Gigasecond
+
+    #set gigasecond constant
+    GIGASECOND = 1000000000
+
+    #method for getting time and adding gigasecond constant
+    def self.from(time_lived)
+        time_lived + GIGASECOND
+    end
 end
